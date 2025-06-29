cc9b039053fdfc37cf400d88e00714dddd98d768
Louis Louisius <macmini@Mac.lan>
2025-06-28 21:40:52 -0400

    Night 5 – VPC, public/private subnets, Cloud NAT

--- infra/prod/main.tf
+++ infra/prod/main.tf
@@ -3,18 +3,43 @@ module "network_base" {
   version = "~> 7.0"
 
   project_id   = var.project_id
-  network_name = "vpc-main"
+  network_name = "vpc-saas-factory"
+  routing_mode = "GLOBAL"
 
   subnets = [
     {
-      subnet_name   = "subnet-public"
-      subnet_ip     = "10.10.10.0/24"
-      subnet_region = var.region
+      subnet_name           = "public-subnet"
+      subnet_ip             = "10.10.10.0/24"
+      subnet_region         = var.region
+      subnet_private_access = "true"
+      subnet_flow_logs      = "true"
     },
     {
-      subnet_name   = "subnet-private"
-      subnet_ip     = "10.10.20.0/24"
-      subnet_region = var.region
+      subnet_name           = "private-subnet"
+      subnet_ip             = "10.10.20.0/24"
+      subnet_region         = var.region
+      subnet_private_access = "true"
+      subnet_flow_logs      = "true"
     }
   ]
+}
+
+resource "google_compute_router" "router" {
+  name    = "cr-vpc-saas-factory"
+  network = module.network_base.network_name
+  region  = var.region
+  project = var.project_id
+}
+
+module "cloud-nat" {
+  source  = "terraform-google-modules/cloud-nat/google"
+  version = "~> 5.0"
+
+  project_id = var.project_id
+  region     = var.region
+  router     = google_compute_router.router.name
+  name       = "nat-gateway"
+
+  log_config_enable = true
+  log_config_filter = "ERRORS_ONLY"
 } 
